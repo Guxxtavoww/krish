41b1536094b4b193fd9f5bd9afbea65f669b3713
Gustavo <galmeida@pdasolucoes.com.br>
2023-03-28 13:51:00 -0300

    adding stuff

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import Logo from '../Logo';
 import { FooterContainer } from './styles';
 
 const Footer: React.FC = () => {
+  const [isPrivacyPolicyModalOpen, setIsPrivacyPolicyOpen] = useState(false);
+
   return (
     <FooterContainer>
-      <div className="te"></div>
+      <button type="button" onClick={() => setIsPrivacyPolicyOpen(true)}>
+        Política de Privacidade
+      </button>
+      <div className="logo_box">
+        <Logo />
+        <span>@{new Date().getFullYear()} Todos os direitos reservados</span>
+      </div>
+      <a
+        href="https://portfolio-gustavo-augusto.netlify.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Desenvolvido por Gustavo
+      </a>
     </FooterContainer>
   );
 };
