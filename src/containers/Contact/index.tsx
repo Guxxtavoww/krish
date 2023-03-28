8b2d6285a6776c281c48c1c9f276bef2e7f02ec9
Gustavo <galmeida@pdasolucoes.com.br>
2023-03-28 11:09:45 -0300

    adding stuff

--- src/containers/Contact/index.tsx
+++ src/containers/Contact/index.tsx
@@ -1,17 +1,40 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 
-import { ContactContainer, ContactInnerContainer } from './styles';
+import { PopUp } from '@/components';
+import { contact_image } from '@/assets';
+
+import {
+  ContactContainer,
+  ContactInnerContainer,
+  ContactWrapper,
+} from './styles';
 
 const Contact: React.FC = () => {
+  const [isContactModalOpen, setIsContactModalOpen] = useState(false);
+
   return (
-    <ContactContainer id="contact">
-      <ContactInnerContainer
-        whileInView={{ opacity: [0, 1], scale: [0.75, 1] }}
-        transition={{ delay: 0.25 }}
-      >
-        <div className="a"></div>
-      </ContactInnerContainer>
-    </ContactContainer>
+    <>
+      <ContactContainer id="contact">
+        <ContactInnerContainer
+          whileInView={{ opacity: [0, 1], scale: [0.75, 1] }}
+          transition={{ delay: 0.25 }}
+        >
+          <ContactWrapper>
+            <h2 className="title">Agende sua avaliação!</h2>
+            <h3 className="subtitle">Edifício Affinity Home Work</h3>
+          </ContactWrapper>
+          <ContactWrapper isImageContainer>
+            <img src={contact_image} alt="Contact Image" />
+          </ContactWrapper>
+        </ContactInnerContainer>
+      </ContactContainer>
+      <PopUp
+        handleClose={() => setIsContactModalOpen(false)}
+        isOpen={isContactModalOpen}
+        closeWhenClickOutside
+        type="whatsapp"
+      />
+    </>
   );
 };
 
--- src/containers/Contact/styles.ts
+++ src/containers/Contact/styles.ts
@@ -1,7 +1,7 @@
 import { motion } from 'framer-motion';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-import { MAIN_BG } from '@/styles/constants';
+import { MAIN_BG, MAIN_TEXT_COLOR } from '@/styles/constants';
 
 export const ContactContainer = styled.section`
   width: 100%;
@@ -16,4 +16,48 @@ export const ContactInnerContainer = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   width: 100%;
+  min-height: 475px;
+
+  @media screen and (max-width: 991px) {
+    grid-template-columns: 1fr;
+    gap: 10px;
+  }
+`;
+
+export const ContactWrapper = styled.div<{ isImageContainer?: boolean }>`
+  display: flex;
+  color: ${MAIN_TEXT_COLOR};
+  ${(props) =>
+    props.isImageContainer
+      ? css`
+          justify-content: flex-start;
+          align-items: flex-end;
+          img {
+            max-width: 100%;
+            object-fit: cover;
+          }
+        `
+      : css`
+          align-items: flex-start;
+          flex-direction: column;
+          gap: 1.25rem;
+          padding: 1.25rem 0;
+        `}
+
+  h2.title {
+    font-size: clamp(1.25rem, 5vmin, 30px);
+    font-weight: 400;
+    text-align: left;
+  }
+
+  h3.subtitle {
+    font-size: clamp(1.25rem, 5vmin, 22px);
+    font-weight: 400;
+    text-align: left;
+  }
+
+  @media screen and (max-width: 991px) {
+    align-items: center;
+    justify-content: center;
+  }
 `;
